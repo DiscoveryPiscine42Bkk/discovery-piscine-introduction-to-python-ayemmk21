df6505e37c4483fb2c2b042dccc634dd8b86d8f5
ayemmk21 <ayemohmohkhin001@gmail.com>
2025-06-24 14:12:09 +0700

    uploaded cell03_ex02

---
+++ cell03/ex02/i_got_that.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+intro = input("What you gotta say? : ")
+while True:
+    next = input("I got that! Anything else? : ")
+    if next == "STOP":
+        break
+    else:
+        continue
